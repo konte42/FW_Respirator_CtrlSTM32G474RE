77072517e86a266e37d420a8b850a5a9302f535f
Matic Pirc <matic.pirc@gmail.com>
2020-04-21 16:48:42 +0200

    Renamed CPAP-PS mode to PS

---
+++ RespiratorApp/Inc/modePS.h
@@ -0,0 +1,17 @@
+/*
+ * modeCPAP_PS.h
+ *
+ * Created: 6. 04. 2020 06:59:15
+ *  Author: maticpi
+ */ 
+
+
+#ifndef MODECPAP_PS_H_
+#define MODECPAP_PS_H_
+
+#include "CommonDefinitions.h"
+#include "ErrorHandling.h"
+
+void modePS(RespSettings_t* Settings, MeasuredParams_t* Measured, CtrlParams_t* Control);
+
+#endif /* MODECPAP_PS_H_ */
