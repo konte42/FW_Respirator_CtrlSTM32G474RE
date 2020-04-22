8db16484c5b2f185b150314c028201870f500158
Matic Pirc <matic.pirc@gmail.com>
2020-04-22 22:43:46 +0200

    Added volume regulation

--- Core/Src/ActuatorControl.c
+++ Core/Src/ActuatorControl.c
@@ -134,7 +134,87 @@ void ActuatorControl(CtrlParams_t* Control, MeasuredParams_t* Measured, RespSett
 			motor_SetSpeed(motorSpeed);
 		break;
 
+// VOLUME REGULATION MODES ///////////////////////////////////////////////////////////
+// dummy regulation for motor start-up - should probably be solved differently
+    case CTRL_PAR_MODE_DUMMY_REGULATE_VOLUME_PID_RESET:
+      PID_fInit(Settings->PID_Volume.P_Factor,
+                Settings->PID_Volume.I_Factor,
+                Settings->PID_Volume.D_Factor,
+                Settings->PID_Volume.maxError,
+                Settings->PID_Volume.maxSumError,
+                PIDdata);
+      Control->mode=CTRL_PAR_MODE_DUMMY_REGULATE_VOLUME;
+      //DO NOT PUT BREAK HERE!
+
+    case CTRL_PAR_MODE_DUMMY_REGULATE_VOLUME:
+      //can only regulate inspiration   //pressure span 50mmH2O --> cca 14500 (14500/16 = cca 900)
+      motorSpeed = PID_fCalculate(1,Control->target_volume, Measured->volume_t, PIDdata);
+      if (Control->target_speed > 0)
+      {
+        if (Control->cur_position < CTRL_PAR_MAX_POSITION)  //Obey if within limits
+        { motor_SetSpeed(Control->target_speed); }
+        else { motor_SetSpeed(0); } //Stop if too far
+      }
+      else // if (Control->target_speed < 0)
+      {
+        if (Control->cur_position > CTRL_PAR_MIN_POSITION)  //Obey if within limits
+        { motor_SetSpeed(Control->target_speed); }
+        else { motor_SetSpeed(0); } //Stop if too far
+      }
+      break;
+
+////////////////// normal volume regulation mode /////////////////////////
+    case CTRL_PAR_MODE_REGULATE_VOLUME_PID_RESET:
+      PID_fInit(Settings->PID_Volume.P_Factor,
+                Settings->PID_Volume.I_Factor,
+                Settings->PID_Volume.D_Factor,
+                Settings->PID_Volume.maxError,
+                Settings->PID_Volume.maxSumError,
+                PIDdata);
+      Control->mode=CTRL_PAR_MODE_REGULATE_VOLUME;
+      //DO NOT PUT BREAK HERE!
+
+    case CTRL_PAR_MODE_REGULATE_VOLUME:
+      //can only regulate inspiration   //pressure span 50mmH2O
+      motorSpeed = PID_fCalculate(1,Control->target_volume, Measured->volume_t, PIDdata);
+      if (Control->cur_position >= CTRL_PAR_MAX_POSITION)
+      {
+        motorSpeed = 0.01;  //minimum speed setting to maintain inhale direction and minimum clamp pressure
+      }
+      if (motorSpeed < 0) motorSpeed = 0.01;  //minimum speed setting to maintain inhale direction and minimum clamp pressure
+      motor_SetSpeed(motorSpeed);
+    break;
+
 // FLOW REGULATION MODES ///////////////////////////////////////////////////////////
+// dummy regulation for motor start-up - should probably be solved differently
+    case CTRL_PAR_MODE_DUMMY_REGULATE_FLOW_PID_RESET:
+      PID_fInit(Settings->PID_Flow.P_Factor,
+                Settings->PID_Flow.I_Factor,
+                Settings->PID_Flow.D_Factor,
+                Settings->PID_Flow.maxError,
+                Settings->PID_Flow.maxSumError,
+                PIDdata);
+      Control->mode=CTRL_PAR_MODE_DUMMY_REGULATE_FLOW;
+      //DO NOT PUT BREAK HERE!
+
+    case CTRL_PAR_MODE_DUMMY_REGULATE_FLOW:
+      //can only regulate inspiration
+      motorSpeed = PID_fCalculate(1,Control->target_flow, Measured->flow, PIDdata);
+      if (Control->target_speed > 0)
+      {
+        if (Control->cur_position < CTRL_PAR_MAX_POSITION)  //Obey if within limits
+        { motor_SetSpeed(Control->target_speed); }
+        else { motor_SetSpeed(0); } //Stop if too far
+      }
+      else // if (Control->target_speed < 0)
+      {
+        if (Control->cur_position > CTRL_PAR_MIN_POSITION)  //Obey if within limits
+        { motor_SetSpeed(Control->target_speed); }
+        else { motor_SetSpeed(0); } //Stop if too far
+      }
+      break;
+
+////////////////// normal flow regulation mode /////////////////////////
 		case CTRL_PAR_MODE_REGULATE_FLOW_PID_RESET:
       PID_fInit(Settings->PID_Flow.P_Factor,
                 Settings->PID_Flow.I_Factor,
